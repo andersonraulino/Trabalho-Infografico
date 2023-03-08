e17e65207cffea40bfa884fcd9f6739d8194bea1
andersonraulino <anderson.raul4631@gmail.com>
2023-03-07 23:25:25 -0300

    Trabalho infográfico

---
+++ script.js
@@ -0,0 +1,82 @@
+// var botoes = document.querySelectorAll("button");
+
+// for (var i = 0; i < botoes.length; i++) {
+//   botoes[i].addEventListener("click", function () {
+//     var balao = document.getElementById("balao");
+//     balao.innerHTML = this.getAttribute("data-info");
+//     balao.style.display = "block";
+//     balao.style.top = this.offsetTop + 50 + "px";
+//     balao.style.left = this.offsetLeft - 100 + "px";
+//   });
+// }
+
+// document.addEventListener("click", function (e) {
+//   var balao = document.getElementById("balao");
+//   if (e.target != balao && e.target.parentNode != balao) {
+//     balao.style.display = "none";
+//   }
+// });
+//
+// BOTAO 1
+var botao = document.getElementById("botao-1");
+
+botao.addEventListener("click", function () {
+  var mensagem = document.getElementById("minhaMensagem");
+  mensagem.innerHTML = "Olá, mundo!";
+});
+// BATAO 2
+var botao = document.getElementById("botao-2");
+
+botao.addEventListener("click", function () {
+  var mensagem = document.getElementById("minhaMensagem");
+  mensagem.innerHTML = "Olá, mundo!22222222222";
+});
+// BOTAO 3
+var botao = document.getElementById("botao-3");
+
+botao.addEventListener("click", function () {
+  var mensagem = document.getElementById("minhaMensagem");
+  mensagem.innerHTML = "Olá, mundo!333333333";
+});
+// BOTAO 4
+var botao = document.getElementById("botao-4");
+
+botao.addEventListener("click", function () {
+  var mensagem = document.getElementById("minhaMensagem");
+  mensagem.innerHTML = "Olá, mundo!44444444444";
+});
+// BOTAO 5
+var botao = document.getElementById("botao-5");
+
+botao.addEventListener("click", function () {
+  var mensagem = document.getElementById("minhaMensagem");
+  mensagem.innerHTML = "Olá, mundo!55555555555";
+});
+// BOTAO 6
+var botao = document.getElementById("botao-6");
+
+botao.addEventListener("click", function () {
+  var mensagem = document.getElementById("minhaMensagem");
+  mensagem.innerHTML = "Olá, mundo!66666666666";
+});
+// BOTAO 7
+var botao = document.getElementById("botao-7");
+
+botao.addEventListener("click", function () {
+  var mensagem = document.getElementById("minhaMensagem");
+  mensagem.innerHTML = "Olá, mundo!7777777777";
+});
+// BOTAO 8
+var botao = document.getElementById("botao-8");
+
+botao.addEventListener("click", function () {
+  var mensagem = document.getElementById("minhaMensagem");
+  mensagem.innerHTML = "Olá, mundo!8888888";
+});
+// BOTAO 9
+var botao = document.getElementById("botao-9");
+
+botao.addEventListener("click", function () {
+  var mensagem = document.getElementById("minhaMensagem");
+  mensagem.innerHTML = "Olá, mundo!999999999";
+});
